94137ad2efffc11cd57d648d4b37ac40b0b3b3b8
Daniel Kunst <s81797@bht-berlin.de>
2023-11-09 20:29:08 +0100

    Test-Eingabefeld ergänzt

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class EingabeFeld extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+  render () {
+    return(<textarea></textarea>);
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Zutaten eingeben:
         </a>
+        <EingabeFeld></EingabeFeld>
       </header>
     </div>
   );
