f9ce95f78e9b20d730e07215f8c785e5369c3347
Daniel Kunst <s81797@bht-berlin.de>
2023-11-15 20:34:45 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './Index.css';
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
