76bb1face52490c3c4bb0b963d82a1bafa1cd4bb
Saskia-Maria Mellies <115100482+saskiamellies@users.noreply.github.com>
2023-11-26 19:46:24 +0100

    Change Navbar Elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,13 @@ export default function NavBar() {
     return <nav className="nav">
         <a href="/" className="home-parge">Home Page</a>
         <ul>
-            <li>
-                <a href="/login">Login</a>
-                <li><a href="/my-receipes">Meine Rezepte</a>
-                    <a href="/my-pantry">Meine Vorratskammer</a>
-                    <a href="/meal-schedule">Mein Essensplaner</a></li>
-            </li>
+          
+                <li><a href="/login">Login</a></li>
+                <li><a href="/login">Login</a></li>
+                <li><a href="/my-receipes">Meine Rezepte</a></li>
+                <li><a href="/my-pantry">Meine Vorratskammer</a></li>
+                <li><a href="/meal-schedule">Mein Essensplaner</a></li>
+           
         </ul>
     </nav>
 }
