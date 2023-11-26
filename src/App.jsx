5b17e9f3c5b45dbe88aa515650033bc1e3a6c8d5
Saskia-Maria Mellies <115100482+saskiamellies@users.noreply.github.com>
2023-11-26 21:34:49 +0100

    Seiten angelegt für die Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link, Navigate} from "react-router-dom";
 import Header from "./components/Header";
 import SearchDish from "./components/SearchDish";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import NavBar from "./components/Navbar";
+import Login from "./components/Login";
+import MeineVorratskammer from "./components/MeineVorratskammer";
+import MeinEssensplaner from "./components/MeinEssensplaner";
 
 function App() {
-  return (
-    <div className="App">
+  /*return (
+
+   <div className="App">
       <Header  />
       <SearchDish />
       <NavBar />
     </div>
-  );
-}
+
+  );*/
+
+  
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <SearchDish />
+          <NavBar />
+        <Link to="/login">Go to Login</Link>
+        <Link to="/my-pantry">Go to My Pantry</Link>
+        <Link to="/meal-schedule">Go to Meal Schedule</Link>
+
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/my-pantry" element={<MeineVorratskammer />} />
+          <Route path="/meal-schedule" element={<MeinEssensplaner />} />
+          {/* Füge weitere Routen hinzu, falls erforderlich */}
+        </Routes>
+      </div>
+    </Router>
+    );
+  }
+
+
 
 export default App;
---
+++ src/components/MeineVorratskammer.jsx
@@ -0,0 +1,3 @@
+export default function MeineVorratskammer(){
+    return <h1>Meine Vorratskammer</h1>
+}
