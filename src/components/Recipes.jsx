16cdac5e4427d63130bc72ce9575f0e88a8ae569
Christoph Rudolph <s82089@bht-berlin.de>
2023-12-03 20:12:03 +0100

    Random Rezept (Button)

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -29,9 +29,27 @@ const Recipes = () => {
     setDetailsOpen(false);
   }
 
+  // Random Rezept Ausgabe - Verknüpfung API
+
+  const handleRandomRecipe = () => {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then((response) => response.json())
+      .then((data) => {
+        const randomDish = data.meals[0];
+        setDishSelected([randomDish]);
+        setDetailsOpen(true);
+      })
+      .catch((error) => {
+        console.error("Fehler beim Abrufen des zufälligen Rezepts:", error);
+      });
+  };
+
+
   return (
     <div className="Recipes">
       <RecipeInput onTyping={handleInputText} placeholder="Bitte Hauptzutat eingeben" />
+      {/* Random Rezept Button */}
+      <button onClick={handleRandomRecipe} className="btn btn-primary">Zufälliges Rezept </button>
       <RecipeList ingredient={ingredient} onClickRecipe={handleClickRecipe} />
       <RecipeDetails isOpen={isDetailsOpen} onClose={closeDetails} recipeDetails={dishSelected} />
     </div>
