3add04c42b383dfbffc187e7e7f04130dd633275
Daniel Kunst <s81797@bht-berlin.de>
2023-12-07 16:13:13 +0100

    Struktur in der style.css angepasst

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -5,15 +5,12 @@ const RecipeDetails = ({ recipeDetails, onClose }) => {
   const meal = recipeDetails[0];
 
   return (
-    <div className="modal-overlay">
-      <div className="recipe-details">
-        <h2>{meal.strMeal}</h2>
-        
-        <button onClick={onClose}>Close</button>
+    <div className="recipe-details">
+      <h2>{meal.strMeal}</h2>
+      <button onClick={onClose}>Close</button>
+      <img src={meal.strMealThumb} alt={meal.strMeal} />
 
-        <img src={meal.strMealThumb} alt={meal.strMeal} />
-
-        <h3>Ingredients:</h3>
+      <h3>Ingredients:</h3>
         <ul>
           {Array.from({ length: 20 }, (_, index) => index + 1)
             .filter((index) => meal[`strIngredient${index}`])
@@ -24,17 +21,13 @@ const RecipeDetails = ({ recipeDetails, onClose }) => {
             ))}
         </ul>
 
-        <h3>Instructions:</h3>
+      <h3>Instructions:</h3>
         <p>{meal.strInstructions}</p>
-
         <div className="video-link">
           {meal.strYoutube && (
             <iframe width="560" height="315" src={meal.strYoutube.replace("watch?v=", "embed/")} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          )
-          } 
+          )} 
         </div>
-
-      </div>
     </div>
   );
 };
