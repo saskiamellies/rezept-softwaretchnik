95e21b24cd6ad0b0b3d8efd967d57313c284a566
Daniel Kunst <s81797@bht-berlin.de>
2023-12-07 15:35:15 +0100

    naming correcture

--- src/components/RecipeInput.jsx
+++ src/components/RecipeInput.jsx
@@ -4,7 +4,7 @@ const RecipeInput = ({ onTyping, placeholder, onClickingRandom }) => {
   return (
     <div className="input-container">
     <input type="text" onChange={(e) => onTyping(e.target.value)} className="form-control" placeholder={placeholder}></input>
-    <button onClick={onClickingRandom} className="btn btn-primary">Gibe me a random recipe!</button>
+    <button onClick={onClickingRandom} className="btn btn-primary">Give me a random recipe!</button>
     </div>
   );
 }
