45654cd2a6e9e8fd3369fcf42eab2e5fc3d81d53
Daniel Kunst <s81797@bht-berlin.de>
2023-12-07 16:01:17 +0100

    update explanation

--- src/components/RecipeInput.jsx
+++ src/components/RecipeInput.jsx
@@ -3,6 +3,7 @@ const RecipeInput = ({ onTyping, placeholder, onClickingRandom }) => {
 
   return (
     <div className="input-container">
+    <h3> You can search for recipes here</h3>
     <input type="text" onChange={(e) => onTyping(e.target.value)} className="form-control" placeholder={placeholder}></input>
     <button onClick={onClickingRandom} className="btn btn-primary">Give me a random recipe!</button>
     </div>
