3c1f9f4d9c1a257a92e784c0563a2f8ea05917c2
Daniel Kunst <s81797@bht-berlin.de>
2023-12-09 17:58:18 +0100

    Test

--- src/components/RecipeInput.jsx
+++ src/components/RecipeInput.jsx
@@ -1,10 +1,15 @@
 
 const RecipeInput = ({ onTyping, placeholder, onClickingRandom }) => {
 
+  {/*onChange={(e) => onTyping(e.target.value)}*/}
+
   return (
     <div className="input-container">
     <h3> You can search for recipes here</h3>
-    <input type="text" onChange={(e) => onTyping(e.target.value)} className="form-control" placeholder={placeholder}></input>
+    <input type="text" id="input" className="form-control" placeholder={placeholder}></input>
+    {/* Testabfrage */}
+    <button onClick={(e) => onTyping(document.getElementById("input").value)} className="btn btn-primary">abschicken</button>
+    {/* Testabfrage */}
     <button onClick={onClickingRandom} className="btn btn-primary">Give me a random recipe!</button>
     </div>
   );
--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 const Recipe = ({ingredient, onClickRecipe}) => {
 
   const [dishes, setDishes] = useState([]);
+  const [dishesComplete, setDishesComplete] = useState([]);
 
   useEffect(() => {
     if (!ingredient) {
@@ -14,10 +15,21 @@ const Recipe = ({ingredient, onClickRecipe}) => {
       .then(response => response.json())
       .then(data => {
         setDishes(data.meals);
-        
       })
+
+      {/* Testabfrage */}
+      let dishesTest = [];
+      dishes?.map((dish) => (
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${dish.idMeal}`)
+        .then(response => response.json())
+        .then(data => {
+          dishesTest.push(data.meals);
+        })
+      ))
+      setDishesComplete(dishesTest);
+      console.log(dishesComplete);
+      {/* Testabfrage */}
     }
-    
   }, [ingredient]);
 
 
@@ -27,13 +39,16 @@ const Recipe = ({ingredient, onClickRecipe}) => {
         <h3>found {dishes.length} recipes:</h3>
       }
       <div className="card-container">
-        {dishes?.map((dish) => (
+      {dishesComplete?.map((dishComplete) => (
+          dishComplete?.map((dish) => (
           <div key={dish.idMeal} className="card">
             <img alt="dish" src={dish.strMealThumb}/>
             <h5 className="card-title">{dish.strMeal}</h5>
+            <h5 className="card-title">{dish.strArea}</h5>
+            <h5 className="card-title">{dish.strCategory}</h5>
             <button onClick={() => onClickRecipe(dish.idMeal)} className="btn btn-primary">open recipe</button>
           </div>
-      ))}
+      ))))}
     </div>
   </div>
   );
