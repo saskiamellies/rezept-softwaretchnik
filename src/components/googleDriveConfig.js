1e6d6ec823a7f27f3073ea7689aaa5a919f72eac
Saskia-Maria Mellies <115100482+saskiamellies@users.noreply.github.com>
2023-12-18 21:05:27 +0100

    Update

---
+++ src/components/googleDriveConfig.js
@@ -0,0 +1,15 @@
+
+const driveConfig = {
+  clientId: '326889312184-9cnsvm9s8kp8ool4b7d3halqo3mv7r2v.apps.googleusercontent.com',
+  developerKey: 'AIzaSyC2KwV3Do-K4eZYUeHBxZYTD26ahGmm3O4',
+  viewId: 'DOCS_IMAGES_AND_VIDEOS',
+  token: '',
+  showUploadFolders: true,
+  showUploadView: true,
+  supportDrives: true,
+  multiselect: true,
+  discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+  scope: 'https://www.googleapis.com/auth/drive.file',
+};
+
+export default driveConfig;
