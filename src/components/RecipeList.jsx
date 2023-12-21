ba63dfaf4cac6bfdef26d3f7298ef6f859e9c5af
Daniel Kunst <s81797@bht-berlin.de>
2023-12-21 12:05:15 +0100

    Kommentare in RecipeList ergänzt

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -5,22 +5,19 @@ const RecipeList = ({ ingredient, isVegetarian, area, onClickRecipe }) => {
   const [dishesUnfiltered, setDishesUnfiltered] = useState([]);
   const [dishes, setDishes] = useState([]);
 
-  /*useEffect(() => {
-    const filteredDishes = filterDishes(dishesUnfiltered);
-    setDishes(filteredDishes);
-  }, [filterDishes, dishesUnfiltered]);*/
-  
+  /*Bei Eingabe einer Zutat werden API-Abfragen ausgeführt und die gefunden Gerichte in einem geschachtelten Array gespeichert*/
    useEffect(() => {
     const fetchData = async () => {
       if (!ingredient) {
         setDishes([]);
         return;
       }
-
+    /*Zuerst werden die Übersichten (entält Name und Bild) aller gefundenen Gerichte mit dieser Zutat abgefragt*/
       try {
         const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
         const data = await response.json();
 
+        /*Dann werden für jedes Element der Abfrage die Details abgefragt (z.B. Zutaten, Anleitung, Video) und jeweils in einem eigenen Array gespeichert */
         if (data.meals) {
           const dishesFetch = await Promise.all(data.meals.map(async (dish) => {
               const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${dish.idMeal}`);
@@ -28,6 +25,7 @@ const RecipeList = ({ ingredient, isVegetarian, area, onClickRecipe }) => {
               return dishData.meals[0];
             })
           );
+          /*Wenn ein Filter gesetzt wurde, wird das gespeicherte Array entsprechend gefiltert*/
           setDishesUnfiltered(dishesFetch);
           const filteredDishes = filterDishes(dishesFetch);
           setDishes(filteredDishes);
@@ -42,6 +40,7 @@ const RecipeList = ({ ingredient, isVegetarian, area, onClickRecipe }) => {
     fetchData();
   }, [ingredient]);
 
+  /*Bei Aktivierung eines Filters (vegetarian, area) wird das gespeicherte Array entsprechend gefiltert*/
   useEffect(() => {
       const filteredDishes = filterDishes(dishesUnfiltered);
       setDishes(filteredDishes);
@@ -49,6 +48,7 @@ const RecipeList = ({ ingredient, isVegetarian, area, onClickRecipe }) => {
   , [isVegetarian, area]);
   
 
+/*Funktion zur Filterung*/
   const filterDishes = (dishesUnfiltered) => {
     const vegetarianFilter = isVegetarian
       ? dishesUnfiltered.filter((dish) => dish.strCategory === 'Vegetarian')
@@ -61,6 +61,8 @@ const RecipeList = ({ ingredient, isVegetarian, area, onClickRecipe }) => {
     return areaFilter;
   };
 
+  /*Ausgabe eines div-Elements je gefundenem Gericht mit den Angaben Name und Bild
+  Bei Klick auf das Element, wird dieses an die Eltern-Komponente überreicht und dort angezeigt*/
   return (
     <div className="output-container">
       {dishes?.length > 0 ? (
