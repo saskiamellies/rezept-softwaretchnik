4ce2cd065d669c1ebf92f9a3f887cf6693b3fb44
Saskia-Maria Mellies <115100482+saskiamellies@users.noreply.github.com>
2023-12-21 09:56:30 +0100

    Update IndexedDB

--- src/components/indexedDBService.js
+++ src/components/indexedDBService.js
@@ -4,13 +4,20 @@ import Dexie from "dexie";
 // define new Database
 const db = new Dexie('MyDatabase');
 db.version(1).stores({
-  myData: '++id, name, email, avatar, customFile',
+  myData: 'firstName, lastName, email, allergies, dietaryRestrictions, favoriteDish, photo',
 });
 
 //add information to profile
 const indexedDBServiceInstance = {
-  addProfile: async (name, email, avatar, customFile) => {
-    return await db.myData.add({ name, email, avatar, customFile });
+  addProfile: async (firstName, lastName, email, allergies, dietaryRestrictions, favoriteDish, photo) => {
+    return await db.myData.add({ 
+      firstName, 
+      lastName, 
+      email, 
+      allergies, 
+      dietaryRestrictions, 
+      favoriteDish, 
+      photo });
   },
   //getting the profile information
   getAllProfiles: async () => {
