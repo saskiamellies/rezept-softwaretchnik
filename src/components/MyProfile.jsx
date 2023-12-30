6ca5904fd90ac51509964dcd83de6ef781739330
Saskia-Maria Mellies <115100482+saskiamellies@users.noreply.github.com>
2023-12-30 23:19:56 +0100

    Update Import

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import * as Sentry from "@sentry/react";
 import { v4 as uuidv4 } from 'uuid';
 import { AvatarGenerator } from 'random-avatar-generator';
 
