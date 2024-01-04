d71f425a46df55aeced9980bc8169044debf990b
Daniel Kunst <s81797@bht-berlin.de>
2024-01-04 09:06:34 +0100

    update

--- src/tests/MyProfile.test.jsx
+++ src/tests/MyProfile.test.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import MyProfile from '../components/MyProfile';
 
 test('renders MyProfile component', () => {
   const { getByText, getByLabelText, queryByText } = render(<MyProfile />);
 
   // Check if the text “Create Your Profile” is present
-  const createProfileText = getByText(/Create Your Profile:/i);
+  const createProfileText = screen.getByText(/Create Your Profile:/i);
   expect(createProfileText).toBeInTheDocument();
 
   // Simulate clicking on the "Update Profile" button if available
-  const updateButton = queryByText(/Update Profile/i);
+  const updateButton = screen.queryByText(/Update Profile/i);
   if (updateButton) {
     fireEvent.click(updateButton);
   }
 
   // Check wether the text "Your Profile:" exists
-  const yourProfileText = getByText(/Your Profile:/i);
+  const yourProfileText = screen.getByText(/Your Profile:/i);
   expect(yourProfileText).toBeInTheDocument();
 
   // Check wether specific Labels are present
-  const firstNameInput = getByLabelText(/First Name:/i);
+  const firstNameInput = screen.getByLabelText(/First Name:/i);
   expect(firstNameInput).toBeInTheDocument();
 
  
