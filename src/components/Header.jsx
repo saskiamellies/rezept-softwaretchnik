b5f7a89eee9bbcf4440dec7c8dc08d2b7eb48178
s82168 <s82168@bht-berlin.de>
2024-01-12 22:36:29 +0100

    logo hinzugefügt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,19 @@ const Header = () => {
   return (
     <div className="header-container">
       <div className="headline-container">
-      <Link to="/recipes" className="display-2 enchanted-eats-link">EnchantedEats</Link>
+      <Link to="/recipes" className="display-2 enchanted-eats-link">
+      <div className="logo-container">
+            <img src="logo2.jpg" alt="EnchantedEats Logo" className="logo" 
+            style={{ width: "70px", height: "auto" }}
+            />
+          </div>
+          EnchantedEats
+        </Link>
       </div>
       <Navbar />
     </div>
   );
 }
+      
 
 export default Header;
