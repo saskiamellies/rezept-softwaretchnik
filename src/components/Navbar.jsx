937a7b60a97810da1220028542f1e8adeaf8426c
s82168 <s82168@bht-berlin.de>
2024-01-16 16:41:24 +0100

    Imprint angepasst und Buttons zentriert in myPantry

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <li><Link to="/myprofile">Profile</Link></li>
         <li><Link to="/mypantry">My Pantry</Link></li>
         <li><Link to="/meal-schedule">Meal Schedule</Link></li>
-        <li><Link to="/impressum">Impressum</Link></li>
+        <li><Link to="/impressum">Imprint</Link></li>
       </ul>
     </nav>
   );
